50601b09f12ee73e5dbb13a8fb599f339771198e
Eliud Heredia <eliudheredia@Eliuds-MBP.fios-router.home>
2021-11-17 10:50:08 -0500

    first commit

---
+++ services/profile-service.js
@@ -0,0 +1,16 @@
+let profile = require('../data/profile.json');
+
+module.exports = (app) => {
+    const getCurrentProfile = (req, res) => {
+        res.json(profile);
+    }
+    app.get('/api/profile', getCurrentProfile)
+
+    const updateCurrentProfile = (req, res) => {
+        profile = {
+            ...req.body
+        }
+        res.json(profile)
+    }
+    app.put('/api/profile', updateCurrentProfile)
+};
